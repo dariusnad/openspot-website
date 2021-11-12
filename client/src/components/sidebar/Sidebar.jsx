54ec2b65a60efdb5433fdf26e2ae7c4bd3bcf23c
cwlui <cwlui@sfu.ca>
2021-11-12 11:06:51 -0800

    basic side bar made

---
+++ client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {slide as Menu} from 'react-burger-menu';
+
+class Sidebar extends React.Component {
+    showSettings (event) {
+      event.preventDefault();
+    }
+  
+    render () {
+      // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
+      return (
+        <Menu className="menu">
+          <a id="home" className="menu-item" href="google.com" >Home</a>
+          {/* <a id="about" className="menu-item" href="/about">About</a>
+          <a id="contact" className="menu-item" href="/contact">Contact</a>
+          <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a> */}
+        </Menu>
+      );
+    }
+  }
+
+export default Sidebar;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,16 @@
 import Topbar from "../../components/topbar/Topbar";
 import Rightbar from "../../components/rightbar/Rightbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+
 import "./home.css"
 
 export default function Home() {
   return (
     <>
       <Topbar />
+      <div className="sidebar">
+        <Sidebar/>
+      </div>
       <div className="homeContainer">
        <Rightbar/>
       </div>
