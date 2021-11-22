a5ad5a76f422328114b1387ea8b42fbb764a0b84
Darius <dnadem@sfu.ca>
2021-11-22 06:49:06 -0800

    Sidebar menu list fixed

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -8,40 +8,65 @@ class Sidebar extends Component {
     super(props);
   
     this.state = {
-      parkingLot : "Kensignton",
-      menuOpen: false
+      curParkingLot : "Kensington",
+      menuOpen: false,
+      parkingLotList : [],
+      menuItems: []
     }
-
     this.changeParkingLot = this.changeParkingLot.bind(this)
+    this.getParkingLots = this.get_parking_lots.bind(this)
   } 
 
+  //Stores selected parking lot
   changeParkingLot(e) {
     var parkingLotName = e.target.innerText;
     this.setState({
-      parkingLot : parkingLotName,
+      curParkingLot : parkingLotName,
       menuOpen : false
-    })
-    console.log(this.state.parkingLot);
+    }, () => console.log(this.state.curParkingLot))
   }
 
+  componentDidMount()
+  {
+        this.get_parking_lots();
+  }
+
+  async get_parking_lots() {
+    axios.get("/map/parking_lots")
+    .then(res => {
+      try {
+        let resParkingLots = []
+        //Push the polygon coordinates into an array
+        for (var i = 0; i < res.data.length; i++) {
+          resParkingLots.push(res.data[i].parkingLotName)
+        }
+        this.setState(
+          {
+            curParkingLot : resParkingLots[0],
+            parkingLotList : resParkingLots
+          });
+      } catch (err) {
+        console.log(err);
+      }
+    })
+  }
+  genMenuList = () => {
+    if(this.state.parkingLotList.length !== 0)
+    {
+      return this.state.parkingLotList.map((m, i) => {
+        return <a onClick={(e) => this.changeParkingLot(e)} className="menu-item" href="/">
+          {this.state.parkingLotList[i]} <br />
+        </a>
+      })
+    }
+  }
+  
   render(){
     return (
       <Menu>
-        <a onClick={(e) => this.changeParkingLot(e)} className="menu-item">
-          Home
-        </a>
-  
-        <a onClick={(e) => this.changeParkingLot(e)} className="menu-item" >
-          About
-        </a>
-  
-        <a onClick={(e) => this.changeParkingLot(e)} className="menu-item" >
-          Services
-        </a>
-  
-        <a onClick={(e) => this.changeParkingLot(e)} className="menu-item" >
-          Contact us
-        </a>
+      <div>
+        {this.genMenuList()}
+      </div>
       </Menu>
     )
   }
